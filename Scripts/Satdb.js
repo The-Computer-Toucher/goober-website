18a53e846b6d7cfd5697ed3ebe8b7bdf8c6a6e43
Bailey M.O. <internetuser130@gmail.com>
2026-04-15 23:49:53 +1000

    Update Satdb.js
    search time!

--- Scripts/Satdb.js
+++ Scripts/Satdb.js
@@ -1,22 +1,80 @@
+let satellites = [];
+
+// fetch data
 fetch("Assets/db/Satdb.json")
 .then(res => res.json())
 .then(sat_data => {
-    let placeholder = document.querySelector("#data-output");
-    let out = "";
-
-    for (let display_data of sat_data) {
-        out += `
-            <tr>
-                <td>${display_data.Entry_ID}</td>
-                <td>${display_data.NORAD_ID}</td>
-                <td>${display_data.COSPAR_ID}</td>
-            </tr>
-        `;
-    }
-
-    placeholder.innerHTML = out;
+    satellites = sat_data;
+    console.log(satellites);
+    displayData(satellites);
 })
 .catch(err => console.error(err));
 
 
+// this function displays the data into the table
+function displayData(data) {
+    let placeholder = document.querySelector("#data-output");
+    let out = "";
 
+    for (let sat of data) {
+        out += ` 
+        <tr>
+            <td>${sat.Entry_ID}</td>
+            <td>${sat.NORAD_ID}</td>
+            <td>${sat.COSPAR_ID}</td>
+
+            <td>${sat.Satellite_Name}</td>
+            <td>${sat.Satellite_alternative_Name}</td>
+            <td> <img class="table_img" src="${sat.Satellite_Image_URL}"> </td>
+            <td>${sat.Satellite_Type}</td>
+            <td>${sat.Satellite_Orbit_Type}</td>
+            <td>${sat.Satellite_Operator}</td>
+
+            <td>${sat.Satellite_Launch_Date}</td>
+            <td>${sat.Satellite_Launch_Vehicle_Name}</td>
+            <td> <img class="table_img" src="${sat.Satellite_Launch_Image_URL}"> </td>
+
+            <td>${sat.Satellite_Mission_Status}</td>
+            <td>${sat.Satellite_Functionality_Status}</td>
+
+            <td>${sat.Satellite_Notes}</td>
+            <td>${sat.Satellite_Tracked}</td>
+            <td>${sat.Satellite_First_Observation_Date}</td>
+        </tr>
+        `;
+    }
+
+    placeholder.innerHTML = out;
+}
+
+// this is the search rules and stuff
+document.querySelector("#search-input").addEventListener("input", function () {
+    let searchValue = this.value.toLowerCase();
+
+    let filtered = satellites.filter(sat => {
+    let combined = `
+        ${sat.Entry_ID}</td>
+        ${sat.NORAD_ID}</td>
+        ${sat.COSPAR_ID}</td>
+
+        ${sat.Satellite_Name}</td>
+        ${sat.Satellite_alternative_Name}</td>
+        ${sat.Satellite_Type}</td>
+        ${sat.Satellite_Orbit_Type}</td>
+        ${sat.Satellite_Operator}</td>
+
+        ${sat.Satellite_Launch_Date}</td>
+        ${sat.Satellite_Launch_Vehicle_Name}</td>
+
+        ${sat.Satellite_Mission_Status}</td>
+        ${sat.Satellite_Functionality_Status}</td>
+
+        ${sat.Satellite_Tracked}</td>
+        ${sat.Satellite_First_Observation_Date}</td>
+    `.toLowerCase();
+
+    return combined.includes(searchValue);
+});
+
+   displayData(filtered);
+});
