83fe8fcfeabdb91abea8c6b9a89b575c2eb1245a
Bailey M.O. <internetuser130@gmail.com>
2026-07-17 15:13:19 +1000

    Delete Scripts/Popup.js

--- Scripts/Popup.js
+++
@@ -1,1 +0,0 @@
-alert('This website was developed on and for PC, the website is best viewed on computers with a resolution of 1920*1080p or higher. ' + 'This website is still under development and is suject to change. ')
